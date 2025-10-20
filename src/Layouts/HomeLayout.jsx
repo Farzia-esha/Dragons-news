13151e457a9007e48deaa68be5866514623cf2a7
Farzia-esha <farziakhandokar24@gmail.com>
2025-10-20 08:57:33 +0600

    layout design & fetch categories

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -3,6 +3,8 @@ import { Outlet } from 'react-router';
 import Header from '../Components/Header';
 import LatestNews from '../Components/LatestNews';
 import Navbar from '../Components/Navbar';
+import LeftAside from '../Components/homelayout/leftAside';
+import RightAside from '../Components/homelayout/RightAside';
 
 const HomeLayout = () => {
     return (
@@ -12,17 +14,21 @@ const HomeLayout = () => {
                 <section className='w-11/12 mx-auto mx-3'>
                     <LatestNews></LatestNews> 
                 </section>
-                <nav>
+                <nav className='w-11/12 mx-auto mx-3'>
                     <Navbar></Navbar>
                 </nav>
             </header>
 
-            <main>
-                <section className='left_nav'></section>
-                <section className='main'>
+            <main className='w-11/12 mx-auto mx-3 grid grid-cols-12'>
+                <aside className='col-span-3'>
+                    <LeftAside></LeftAside>
+                </aside>
+                <section className='main col-span-6'>
                     <Outlet></Outlet>
                 </section>
-                <section className='right_nav'></section>
+                <aside className='col-span-3'>
+                    <RightAside></RightAside>
+                </aside>
             </main>
         </div>
     );
