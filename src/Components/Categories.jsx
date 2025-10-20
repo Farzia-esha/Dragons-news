5869fb4c9cf6c45b8ba121ca08e4767e8927a2e7
Farzia-esha <farziakhandokar24@gmail.com>
2025-10-20 11:20:44 +0600

    show All category & login

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
-const categoryPromise =fetch('categories.json')
+import { NavLink } from 'react-router';
+const categoryPromise =fetch('/categories.json')
     .then((res) => res.json());
 
 const Categories = () => {
@@ -8,6 +9,17 @@ const Categories = () => {
     return (
         <div>
             <h2 className='font-bold'>All Categories ({categories.length})</h2>
+            <div className='grid grid-cols-1 mt-5 gap-2'>
+                {categories.map(category=>(
+                    <NavLink
+                        key={category.id}
+                        className={'btn bg-base-100 border-0 hover:bg-base-200 font-semibold text-accent '}
+                        to={`/category/${category.id}`}
+                    >
+                        {category.name}
+                    </NavLink>
+                ))}
+            </div>
         </div>
     );
 };
